cd82676fc267b4b6604ef0ec0580b94eabb843b3
Luis-Miguelj <luismiguel.dev01@gmail.com>
2025-06-09 00:09:34 -0300

    Fazendo alterações de parametros nas rotas

--- src/domains/endpoints/users/get-user-id.ts
+++ src/domains/endpoints/users/get-user-id.ts
@@ -5,33 +5,37 @@ import { t } from 'elysia'
 const users = new User()
 
 export const getUserId = server.get(
-  '/users/:id',
-  async ({ params, status }) => {
-    const { id } = params
-    const user = await users.getUserById(id)
+  '/users',
+  async ({ status, jwt, request }) => {
+    const token = request.headers.get('Authorization')
+    const verify = await jwt.verify(token as string)
+    if (!verify || !verify.sub) {
+      return status(400, 'Token inválido ou expirado')
+    }
+    const user = await users.getUserById(verify.sub)
 
     if (!user || (!user.usuario && user.message)) {
       return status(404, user?.message || 'Usuário não encontrado')
     }
 
     if (user.usuario) {
-      return status(200, user.usuario)
+      return status(200, {
+        id: user.usuario.id,
+        name: user.usuario.name,
+        createdAt: user.usuario.createdAt,
+      })
     }
 
     // Fallback (evita retornar undefined)
     return status(404, 'Usuário não encontrado')
   },
   {
-    params: t.Object({
-      id: t.String(),
-    }),
     response: {
+      400: t.String(),
       404: t.String(),
       200: t.Object({
         id: t.String(),
         name: t.String(),
-        email: t.String(),
-        password: t.String(),
         createdAt: t.Date(),
       }),
     },
